775a1388d6da1a7cb74aaef7d6fdb7965d87821d
Nonso Egwuagha <egwuaghanonso@gmail.com>
2023-04-15 12:31:24 +0100

    made it responsive

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="navbar">
     <img src="../assets/logo.svg" class="logo" />
- 
-    <ul>
+
+    <ul class="desktop">
       <li>Home</li>
       <li>New</li>
       <li>Popular</li>
@@ -10,19 +10,29 @@
       <li>Categories</li>
     </ul>
 
-    <img src="../assets/icon-menu.svg" class="mobile-menu"/>
+    <img
+      src="../assets/icon-menu.svg"
+      class="mobile-menu"
+      @click="handleMenu"
+    />
 
-    <div class="mobile-list" >
-        <ul>
-          <img src="../assets/icon-menu-close.svg" style="color: black; font-size: 2rem"/>
-          <li>Home</li>
-          <li>New</li>
-          <li>Popular</li>
-          <li>Trending</li>
-          <li>Categories</li>
-        </ul>
-      </div>
+    <div class="mobile-list" v-show="menu">
+      <ul class="mobile">
+        <img
+          class="mob_menu_close"
+          src="../assets/icon-menu-close.svg"
+          style="color: black; font-size: 2rem"
+          @click="handleMenu"
+        />
+        <li>Home</li>
+        <li>New</li>
+        <li>Popular</li>
+        <li>Trending</li>
+        <li>Categories</li>
+      </ul>
+    </div>
   </div>
+  <div class="overlay" v-show="menu"></div>
 </template>
 
 <style scoped>
@@ -33,21 +43,18 @@
   height: 15vh;
   align-items: center;
   color: grey;
-  
+
   /* font-family:'Inter', sans-serif ; */
 }
-.navbar .logo{
+.navbar .logo {
   width: 6%;
   height: 35%;
-  
 }
-
 
 .navbar ul {
   display: flex;
   list-style: none;
   margin-right: 15%;
-  
 }
 
 .navbar ul li {
@@ -62,66 +69,85 @@
   color: hsl(5, 85%, 63%);
 }
 
-.navbar .mobile-menu{
+.navbar .mobile-menu {
   display: none;
 }
-.navbar .mobile-list{
+.navbar .mobile-list {
   display: none;
 }
 
-
-
-@media (max-width: 800px){
-  .navbar{
-    height:10vh ;
+@media (max-width: 800px) {
+  .overlay {
+    position: absolute;
+    top: 0%;
+    left: 0%;
+    width: 100%;
+    height: 285vh;
+    background-color: rgba(0, 0, 0, 0.6);
+    z-index: 5;
+  }
+  .navbar {
+    height: 10vh;
     background-color: white;
   }
-  .navbar .logo{
+  .navbar .logo {
     margin-left: 3%;
     width: 9%;
   }
 
-  .navbar ul li{
+  .desktop li {
     display: none;
-
   }
-  .navbar .mobile-menu{
+  .navbar .mobile-menu {
     display: flex;
     margin-right: 3%;
-    
-  height: 30%;
+    height: 30%;
   }
 
-   .navbar .mobile-list {
+  .navbar .mobile-list {
     display: flex;
     flex-direction: column;
-    background-color: rgba(0, 0, 0, 0.5);
-    min-height: 410%;
-    position: absolute;
-    width: 100vw;
-    top: -100%;
-    padding: 0;
-    z-index: 500;
-    margin: 0;
-  }
-   .navbar .mobile-list ul {
-    flex-direction: column;
-    margin-left:25%;
     background-color: white;
-    justify-content: flex-start;
-    width: 60vw;
-    height: 350vh;
-    padding-right: 6vw;
-    padding-top: 10vh;
-   
-  }
-
- .navbar .mobile-list ul li {
-    font-weight: 700;
+    position: absolute;
+    min-height: 191vh;
+    width: 70%;
+    z-index: 10;
+    right: 0;
+    justify-content: center;
     color: black;
-    list-style: none;
-    margin-bottom: 20%;
   }
 
+  .mob_menu_close {
+    position: absolute;
+    right: 10%;
+    top: 50%;
+  }
+
+  .mobile {
+    display: flex;
+
+    width: 10%;
+    height: 80%;
+
+    flex-direction: column;
+    gap: 3rem;
+    justify-content: center;
+    margin-top: 35rem;
+  }
 }
 </style>
+
+<script>
+export default {
+  data() {
+    return {
+      menu: false,
+    };
+  },
+  methods: {
+    handleMenu() {
+      this.menu = !this.menu;
+    },
+  },
+};
+</script>
